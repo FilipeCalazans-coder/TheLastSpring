e362ed8d28137e559f0d23d890a7890286366a4e
FilipeCalazans-coder <filipemariz1@gmail.com>
2026-02-25 20:25:33 -0300

    Initial Commit

---
+++ Assets/Scripts/EnemyIA.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyIA : MonoBehaviour
+{
+
+    private enum State
+    {
+        Roaming
+    }
+
+    private State state;
+    private EnemyPathFinding enemyPathFinding;
+
+    private void Awake()
+    {
+        enemyPathFinding = GetComponent<EnemyPathFinding>();
+        state = State.Roaming;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(RoamingRoutine());
+    }
+
+    private IEnumerator RoamingRoutine()
+    {
+        while (state == State.Roaming)
+        {
+            Vector2 roamPosition = GetRoamingPosition();
+            enemyPathFinding.MoveTo(roamPosition);
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private Vector2 GetRoamingPosition()
+    {
+        return new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+
+}
