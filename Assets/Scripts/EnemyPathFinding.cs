48fe8c2d4174e776d5b52d923d16aa20c1f2700f
FilipeCalazans-coder <filipemariz1@gmail.com>
2026-03-03 22:36:44 -0300

    Sistema de Combate
    -- Vida do inimigo
    -- KnockBack
    -- Dano de arma
    
    --Provavelmente tem mais coisa, mas eu não lembro

--- Assets/Scripts/EnemyPathFinding.cs
+++ Assets/Scripts/EnemyPathFinding.cs
@@ -8,14 +8,17 @@ public class EnemyPathFinding : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 moveDir;
+    private KnockBack knockBack;
 
     private void Awake()
     {
+        knockBack = GetComponent<KnockBack>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
+        if (knockBack.gettingKnockedBack) { return;}
         rb.MovePosition(rb.position + moveDir * (moveSpeed * Time.fixedDeltaTime));
     }
 
